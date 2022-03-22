479f677bdf953365b8b5b76f15fd130f3ccd82fd
gguerran <gustavoguerra.gr@gmail.com>
2022-03-22 14:34:34 -0300

    feat[#43] - movendo icones para imagens

--- demeter_app/lib/core/app_assets.dart
+++ demeter_app/lib/core/app_assets.dart
@@ -4,9 +4,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class AppAssets {
   static final avatar = Image.asset('images/avatar.jpeg', fit: BoxFit.fill);
-  static final dashboardMenu = SvgPicture.asset("icons/dashboard.svg");
-  static final hrMenu = SvgPicture.asset("icons/hr.svg");
-  static final refundMenu = SvgPicture.asset("icons/refund.svg");
-  static final historicMenu = SvgPicture.asset("icons/clock.svg");
-  static final moreMenu = SvgPicture.asset("icons/more.svg");
+  static final dashboardMenu = SvgPicture.asset("images/icons/dashboard.svg");
+  static final hrMenu = SvgPicture.asset("images/icons/hr.svg");
+  static final refundMenu = SvgPicture.asset("images/icons/refund.svg");
+  static final historicMenu = SvgPicture.asset("images/icons/clock.svg");
+  static final moreMenu = SvgPicture.asset("images/icons/more.svg");
 }
