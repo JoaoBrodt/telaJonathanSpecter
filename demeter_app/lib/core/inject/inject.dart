3062444570a55f649cf21dd020bf10af5abb0b9e
Cássio Sousa <cassio.sousa@accurate.com.br>
2022-03-23 11:39:29 -0300

    feat[#36] adição de configurações de ambientes com .env

--- demeter_app/lib/core/inject/inject.dart
+++ demeter_app/lib/core/inject/inject.dart
@@ -1,9 +1,6 @@
-import 'package:demeter_app/core/services/dio_http_service_impl.dart';
 import 'package:demeter_app/core/services/http_service.dart';
 import 'package:demeter_app/core/services/mock_http_service_impl.dart';
 import 'package:demeter_app/features/refund/domain/export.dart';
-import 'package:demeter_app/features/refund/domain/repository/search_refund_repository.dart';
-import 'package:demeter_app/features/refund/domain/repository/search_refund_repository_impl.dart';
 import 'package:get_it/get_it.dart';
 
 class Inject {
